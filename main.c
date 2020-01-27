0940cb945a687badab9e336e7aefd474d9338fc3
Jan Riebe <corone.et.monedula@gmail.com>
2020-01-27 08:32:16 +0000

    Adding keyboard input functions.

--- main.c
+++ main.c
@@ -6,6 +6,11 @@ void InitializeEngine(void);
 void UpdateFixed(int);
 void Update(float);
 void CleanUp(void);
+void KeyPressed(unsigned char key, int x, int y);
+void KeyUp(unsigned char key, int x, int y);
+void RequestToCloseEngine();
+void KeySpecial(int key, int x, int y);
+
 
 int FIXED_UPDATE_TIMESTEP = 1000;
 
@@ -31,6 +36,9 @@ void InitializeEngine()
     glutTimerFunc(0, UpdateFixed, 0);
     glutCloseFunc(CleanUp);
     setPerFrameCallback(Update);
+    glutKeyboardFunc(KeyPressed);
+	glutKeyboardUpFunc(KeyUp);
+    glutSpecialFunc(KeySpecial);
 }
 
 
@@ -45,7 +53,7 @@ void UpdateFixed(int Value)
 
     }
     glutTimerFunc(FIXED_UPDATE_TIMESTEP, UpdateFixed, 1);
-    printf("%s\n","Update Fixed");
+    //printf("%s\n","Update Fixed");
 }
 
 /*
@@ -53,9 +61,59 @@ void UpdateFixed(int Value)
 */
 void Update(float deltaTime)
 {
-    printf("%f\n", deltaTime);
+    //printf("%f\n", deltaTime);
 }
 
+
+void KeyPressed(unsigned char key, int x, int y)
+{
+    switch(key)
+	{
+
+	// Escape
+	case 27 :
+		RequestToCloseEngine();
+		break;
+
+    default :
+        printf("Pressed key: %c\nAt position: %d,%d\n", (char)key,x,y);
+		break;
+	}
+}
+
+void KeyUp(unsigned char key, int x, int y)
+{
+    printf("Lifted key: %c\nAt position: %d,%d\n", (char)key,x,y);
+}
+
+void KeySpecial(int key, int x, int y)
+{
+    switch(key)
+	{
+    case GLUT_KEY_UP :
+		printf("Up\n");
+		break;
+
+    case GLUT_KEY_DOWN :
+		printf("Down\n");
+		break;
+
+    case GLUT_KEY_LEFT :
+		printf("Left\n");
+		break;
+
+    case GLUT_KEY_RIGHT :
+		printf("Right\n");
+		break;
+    }
+}
+
+
+
+void RequestToCloseEngine()
+{
+    glutLeaveMainLoop();
+}
 
 
 void CleanUp()
