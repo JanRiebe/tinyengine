544d9859b5cdd70bb4da8ca1955f657ac7d0d3f5
Jan Riebe <corone.et.monedula@gmail.com>
2022-01-15 08:31:55 +0100

    Separating input into module

--- main.cpp
+++ main.cpp
@@ -51,9 +51,6 @@ void InitializeEngine()
     glutTimerFunc(0, UpdateFixed, 0);
     glutCloseFunc(CleanUp);
     renderer->setPerFrameCallback(Update);
-    glutKeyboardFunc(KeyPressed);
-	  glutKeyboardUpFunc(KeyUp);
-    glutSpecialFunc(KeySpecial);
 }
 
 
@@ -102,51 +99,6 @@ void Update(float deltaTime)
 }
 
 
-
-void KeyPressed(unsigned char key, int x, int y)
-{
-    switch(key)
-	{
-
-	// Escape
-	case 27 :
-		RequestToCloseEngine();
-		break;
-
-    default :
-        printf("Pressed key: %c\nAt position: %d,%d\n", (char)key,x,y);
-		break;
-	}
-}
-
-void KeyUp(unsigned char key, int x, int y)
-{
-    printf("Lifted key: %c\nAt position: %d,%d\n", (char)key,x,y);
-}
-
-void KeySpecial(int key, int x, int y)
-{
-    switch(key)
-	{
-    case GLUT_KEY_UP :
-		printf("Up\n");
-		break;
-
-    case GLUT_KEY_DOWN :
-		printf("Down\n");
-		break;
-
-    case GLUT_KEY_LEFT :
-		printf("Left\n");
-    rootNode->children.back()->transform->position.x-=0.1;
-		break;
-
-    case GLUT_KEY_RIGHT :
-		printf("Right\n");
-    rootNode->children.back()->transform->position.x+=0.1;
-		break;
-    }
-}
 
 
 
